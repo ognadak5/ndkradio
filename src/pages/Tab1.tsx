0dafdccf47eeeeb67f17cbc1352c9b39c1c6abcc
nadak <ognadak5@gmail.com>
2021-01-20 05:27:02 +0200

    Basic Player UI Upgrade

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonProgressBar, IonIcon } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+import { musicalNotesOutline } from 'ionicons/icons';
+
 const Tab1: React.FC = () => {
+
+  var audio = new Audio('https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3?1611009242');
+
+  function player(){
+
+    if( audio.paused ){
+      audio.play();
+      console.log('Playing');
+    }else{
+      audio.pause();
+      console.log('Paused');
+    }
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -15,11 +31,17 @@ const Tab1: React.FC = () => {
 
         <div className="main">
           <strong>Nadak Radio</strong>
+          <br/>
+          <br/>
 
-          <audio id='music' controls>
-            <source src="https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3?1611009242" type="audio/mpeg"></source>
-            Your browser does not support the audio element.
-          </audio>
+          <div className='progressbar'>
+            <p>Streaming...</p>
+            <IonProgressBar buffer={0.1} ></IonProgressBar><br />
+          </div>
+
+          <IonButton size="large" fill='clear' onClick={() =>{ player() }}>
+            <IonIcon icon={musicalNotesOutline}></IonIcon>
+          </IonButton>
         </div>
 
       </IonContent>
